bc39b8f17a070eb8ea9f14bf552cb82ec0c006e3
JamshidSherkulov <jamshidsherkhulov@gmail.com>
2023-02-26 13:15:06 +0500

    finshed users result

--- src/components/Registered/Information.jsx
+++ src/components/Registered/Information.jsx
@@ -3,17 +3,17 @@ import './Registered.css'
 import {FiUsers} from 'react-icons/fi'
 import {AiOutlineRise} from 'react-icons/ai'
 
-const Information = () => {
+const Information = (props) => {
 
     return(
         <div>
             <div className="d-flex justify-content-between align-items-center">
-                <p>Faol foydalanuvchilar</p>
-                <p><FiUsers className="text-primary fs-4" /></p>
+                <p>{ props.title }</p>
+                <p>{ props.icons }</p>
             </div>
-            <h3 className="text-center fw-bold">1.5 mln</h3>
+            <h3 className="text-center fw-bold">{ props.number }</h3>
             <div className="d-flex justify-content-between align-items-center">
-                <p className="text-success"><AiOutlineRise /> +15%</p>
+                <p className="text-success">{ props.resultIcon } {props.result}</p>
                 <p><a href="#">Batafsil</a></p>
             </div>
         </div>
--- src/components/Registered/Registered.jsx
+++ src/components/Registered/Registered.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import './Registered.css'
-import { FiUsers } from 'react-icons/fi'
-import { AiOutlineExclamationCircle } from 'react-icons/ai'
+import { FiUsers, FiUserX } from 'react-icons/fi'
+import { AiOutlineExclamationCircle, AiFillQuestionCircle, AiOutlineCloudDownload } from 'react-icons/ai'
+import {FaUserCheck} from 'react-icons/fa'
+import {GrUserPolice} from 'react-icons/gr'
+import {AiOutlineRise} from 'react-icons/ai'
+import {BiTrendingDown} from 'react-icons/bi'
 import Information from "./Information";
 
 const Registered = () => {
@@ -10,28 +14,28 @@ const Registered = () => {
         <div className="mt-5 container mx-auto">
             <h3>Ro’yxatdan o’tanlar</h3>
             <div id="all__users" className="border p-3 text-center">
-                <span><FiUsers className="display-2 text-primary" /></span>
-                <span className="text-primary display-1 fw-bold">1 712 001  </span>
-                <span><AiOutlineExclamationCircle className="display-2 text-primary" /></span>
+                <div><FiUsers className="display-2" /></div>
+                <div className="text-primary display-1 fw-bold">1 712 001  </div>
+                <div><AiOutlineExclamationCircle className="display-2 text-primary"/></div>
             </div>
             <div className="row justify-content-between align-items-center">
-                <div className="col-3 border m-3 p-4">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Faol foydalanuvchilar' icons={ <FiUsers className="text-primary fs-4" /> } number='1.5 mln' result='+15%' resultIcon={<AiOutlineRise className="text-success" />} />
                 </div>
-                <div className="col-3 border m-3 p-4">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Shaxsini tasdiqlagan' icons={<FaUserCheck className="fs-4 text-success" />} number='625.1k' result='+15%' resultIcon={<AiOutlineRise className="text-success" />} />
                 </div>
-                <div className="col-3 border m-3 p-4">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Shubhali foydalanuvchi' icons={<AiFillQuestionCircle className="fs-4 text-danger" />} number='5.1k' result='-3.5%' resultIcon={<BiTrendingDown className="text-danger" />} />
                 </div>
-                <div className="col-3 border m-3 p-4">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Faol bo’lmagan' icons={<FiUserX className="fs-4 text-danger" />} number='25.1k' result='+15%' resultIcon={<AiOutlineRise className="text-success" />} />
                 </div>
-                <div className="col-3 border m-3">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Soliq xodimlari' icons={<GrUserPolice className="fs-4 text-primary" />} number='2.1k' result='+5%' resultIcon={<AiOutlineRise className="text-success" />} />
                 </div>
-                <div className="col-3 border m-3 p-4">
-                    <Information />
+                <div className="col-3 border m-3 p-3">
+                    <Information title='Yuklab olingan' icons={<AiOutlineCloudDownload className="fs-4 text-warning" />} number='1.7 mln' result='+5%' resultIcon={<AiOutlineRise className="text-success" />} />
                 </div>
                
             </div>
